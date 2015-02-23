b8b844a5992d869de376c81fc8debc78f53ed109
Aedan Dispenza <aedan.dispenza@gmail.com>
2015-02-23 17:48:20 -0500

    HERE COMES THE MONEYYYYYY

--- cash.js
+++ cash.js
@@ -10,8 +10,8 @@ $(document).ready(function(){
     $(".cash").each(function(i){
         var rotationNum=Math.round((Math.random()*720)+1);
         var rotation="rotate("+rotationNum+"deg)";
-        var posx = Math.round(Math.random() * ww)-20;
-        var posy = Math.round(Math.random() * wh)-20;
+        var posx = Math.round(Math.random() * ww)-50;
+        var posy = Math.round(Math.random() * wh)-50;
         $(this).css("top", posy + "px").css("left", posx + "px").css("transform",rotation).css("-ms-transform",rotation).css("-webkit-transform",rotation);
     });
     //they spinnin' baby
